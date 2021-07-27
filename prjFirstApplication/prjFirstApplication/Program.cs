c2f433b155763914586ee77a48a837ae49d6102a
monishanandhakumar <57033484+monishanandhakumar@users.noreply.github.com>
2021-07-27 14:36:11 +0530

    updated program.cs
    add cw

--- prjFirstApplication/prjFirstApplication/Program.cs
+++ prjFirstApplication/prjFirstApplication/Program.cs
@@ -7,6 +7,8 @@ namespace prjFirstApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+             Console.WriteLine("Hi All!");
         }
     }
 }
